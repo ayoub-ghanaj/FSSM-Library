287c140f043a78e58cd5b54aee11ab507283b75e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-10 02:12:09 +0100

    login done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,27 @@
 package src;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main extends Application  {
-
+    private double xOffset = 0;
+    private double yOffset = 0;
     Stage window ;
     Scene login , books, users, history ;
     Button button;
@@ -24,39 +32,63 @@ public class Main extends Application  {
 
     @Override
     public void start(Stage stage) throws Exception {
-        window = stage;
+
         SQLiteTools sql = new SQLiteTools();
+        window = stage;
+
+        // Create a GridPane layout
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        // Add a label for the username field
+        Label usernameLabel = new Label("Login :");
+        grid.add(usernameLabel, 0, 1);
+
+        // Add a text field for the username
+        TextField usernameField = new TextField();
+        grid.add(usernameField, 1, 1);
+
+        // Add a label for the password field
+        Label passwordLabel = new Label("Password :");
+        grid.add(passwordLabel, 0, 2);
+
+        // Add a password field for the password
+        PasswordField passwordField = new PasswordField();
+        grid.add(passwordField, 1, 2);
+
+        // Add a login button
+        Button loginButton = new Button("Submit");
+        loginButton.setAlignment(Pos.CENTER);
+        grid.add(loginButton, 1, 3);
+
+        // Add a label for the error field
+        Label errorLabel = new Label(" Invalid Login/Password ");
+        grid.add(errorLabel, 0, 3);
+        errorLabel.setVisible(false);
+
+        loginButton.setId("loginbtn");
+
+
+
+
+
+
         ResultSet rs = sql.displayStudents();
 
-        GridPane log_gp = new GridPane();
-        log_gp.setPadding(new Insets(12,12,12,12) );
-        log_gp.setVgap(9);
-        log_gp.setHgap(11);
-
-
-        Label login_label = new Label("cni :");
-        log_gp.setConstraints(login_label,0,0);
-
-        TextField cni_input = new TextField();
-        log_gp.setConstraints(cni_input,1,0);
-
-        Label pass_label = new Label("password :");
-        log_gp.setConstraints(pass_label,0,1);
-
-        TextField pass_input = new TextField();
-        log_gp.setConstraints(pass_input,1,1);
-
-        button = new Button("login");
-        log_gp.setConstraints(button,1,2);
-
-        button.setOnAction(e->{
-            String username = cni_input.getText().trim();
-            String password = pass_input.getText().trim();
+//        button.setStyle("-fx-focus-color: transparent;");
+        loginButton.setOnAction(e->{
+            errorLabel.setVisible(false);
+            String username = usernameField.getText().trim();
+            String password = passwordField.getText().trim();
             if(username.length()>0 && password.length()>0 ){
                 try {
                     int[] arr = sql.login(username,password);
                     if(arr[0]==0){
                         System.out.println("makainch");
+                        errorLabel.setVisible(true);
                     }else if(arr[0]==1){
                         System.out.println("admin w  id dialo :" + arr[1]);
                     }else{
@@ -69,16 +101,64 @@ public class Main extends Application  {
                 }
             }
         });
-        window.setTitle("Login");
-        log_gp.getChildren().addAll(login_label,cni_input,pass_label,pass_input,button);
-        Scene scene = new Scene(log_gp,300,200);
-        scene.getStylesheets().add(getClass().getResource("FSLib.css").toString());
 
+//        Button ext
+
+        BorderPane home_pan = new BorderPane();
+        home_pan.setCenter(grid);
+
+        // Create the scene and add the home_pan
+        Scene scene = new Scene(home_pan, 420, 220);
+        window.initStyle(StageStyle.UNDECORATED);
+
+        //resizable/movable
+        scene.setOnMousePressed(e-> {
+                xOffset = e.getSceneX();
+                yOffset = e.getSceneY();
+
+        });
+        scene.setOnMouseDragged(e->{
+                stage.setX(e.getScreenX() - xOffset);
+                stage.setY(e.getScreenY() - yOffset);
+
+        });
+
+        // top bar buttons
+        final int height = 25;
+        HBox top_items = new HBox();
+        Button close = new Button("x");
+        close.setId("exit-btn");
+        close.setOnAction(e ->{
+            Platform.exit();
+        });
+//        Button fullscreen = new Button("■");
+        Button minimze = new Button("-");
+        minimze.setId("minimize-btn");
+        minimze.setOnAction(e ->{
+            window.setIconified(true);
+        });
+        
+        top_items.getChildren().addAll(minimze,close);
+        // Align the child nodes on the right side of the HBox
+        top_items.setPrefWidth(window.getWidth());
+        top_items.setPrefHeight(height);
+        top_items.setMinHeight(height);
+        top_items.setMaxHeight(height);
+        top_items.setSpacing(1.5);
+        top_items.setAlignment(Pos.BASELINE_RIGHT);
+
+        home_pan.setTop(top_items);
+
+        window.setMaxWidth(470);
+        window.setMaxHeight(240);
+        window.setMinWidth(420);
+        window.setMinHeight(220);
         window.setScene(scene);
-
         window.show();
-
-
+        scene.getStylesheets().add(getClass().getResource("FSLib.css").toString());
+        errorLabel.setStyle("-fx-font-size: 11px; -fx-text-fill: red;");
+        window.setTitle("Login");
+        window.show();
 
     }
 
